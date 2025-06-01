f58259d4f8350483e2f2f2222f440c9c1da59d74
elthonJhon1906 <elthonjhonkevin@gmail.com>
2025-06-01 14:27:26 +0700

    fourth

--- fix1.ino
+++ fix1.ino
@@ -1,51 +1,56 @@
 #include <DHT.h> 
 #include <LiquidCrystal_I2C.h> 
 #include <Wire.h> 
-
 #define DHTPIN 7 
 #define soilMoisture A0
 #define buzzer 8
 #define relayPin 10
 #define DHTTYPE DHT11 
-
-DHT dht(DHTPIN, DHTTYPE); 
+DHT dht (DHTPIN, DHTTYPE); 
 LiquidCrystal_I2C lcd(0x27, 16, 2);  
-
+ 
 void setup() { 
+  // put your setup code here, to run once: 
   lcd.backlight(); 
   Serial.begin(9600); 
   dht.begin();  
   lcd.init(); 
   lcd.clear(); 
-
-  pinMode(buzzer, OUTPUT);
-  pinMode(relayPin, OUTPUT);
 }
 
 void loop() { 
+// put your main code here, to run repeatedly: 
   float t = dht.readTemperature(); 
   float h = dht.readHumidity(); 
   int soilValue = analogRead(soilMoisture);
-  soilValue = map(soilValue, 10, 550, 100, 0); // adjusted mapping
+  soilValue = map(soilValue, 550, 10, 0, 100);
 
-  Serial.print("Suhu : "); Serial.print(t); Serial.println("°C"); 
-  Serial.print("Kelembapan : "); Serial.print(h); Serial.println("%"); 
-  Serial.print("Moisture : "); Serial.print(soilValue); Serial.println("%");
-
-  lcd.setCursor(0, 0); 
-  lcd.print("Suhu: "); lcd.print(t); lcd.print("C "); 
-  lcd.setCursor(0, 1); 
-  lcd.print("Moist: "); lcd.print(soilValue); lcd.print("%");
-
-  if (soilValue < 20 && t > 25) {
-    tone(buzzer, 1000);
-    digitalWrite(relayPin, LOW); // aktifkan relay
-    delay(1000);
-    noTone(buzzer);
-    delay(3000);
-  } else {
-    digitalWrite(relayPin, HIGH); // matikan relay
-  }
-
+  Serial.print("Suhu : "); 
+  Serial.print(t); 
+  Serial.println("°C"); 
+  Serial.print("Kelembapan : "); 
+  Serial.print(h); 
+  Serial.println("%"); 
+  lcd.setCursor(0,0); 
+  lcd.print("Suhu : "); 
+  lcd.print(t);
+  lcd.setCursor(0,1); 
+  lcd.print("Kelembapan: "); 
+  lcd.print(h); 
+  delay(2000); 
+  lcd.clear();
+  lcd.setCursor(0,1); 
+  lcd.print("Mositure : ");
+  lcd.print(soilValue);
+  if(soilValue < 20 && t > 25) {
+  tone(buzzer, 1000);
+  digitalWrite(relayPin, LOW);
   delay(1000);
+  noTone(buzzer);
+  delay(3000);
+  } else {
+  digitalWrite(relayPin, HIGH);  
+  }
+  delay(1000);
+
 }
